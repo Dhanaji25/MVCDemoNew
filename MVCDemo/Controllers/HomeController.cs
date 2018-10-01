fb86ccaab6099c9f5b11ae070a96e380692956a6
Dhanaji25 <dhanajim25@gmail.com>
2018-10-01 14:20:21 +0530

    First Commit

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(addModel model)
+        {
+            addModel mod = new addModel();
+            if (ModelState.IsValid)
+            {
+                mod.inputOne = model.inputOne;
+                mod.inputTwo = model.inputTwo;
+                mod.result= model.inputOne + model.inputTwo;
+            }
+            return View(mod);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Models/addModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class addModel
+    {
+
+        [Required(ErrorMessage = "Input one is required")]
+
+        public int inputOne { get; set; }
+        [Required(ErrorMessage = "Input two is required")]
+        public int inputTwo { get; set; }
+        public int? result { get; set; }
+    }
+}
